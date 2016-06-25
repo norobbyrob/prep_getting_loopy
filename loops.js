13a200e16f3acae74214debf4cf43a2ebdecd3ac
norobbyrob <nrobinson18@punahou.edu>
2016-06-24 14:35:53 -1000

    first challenge finished

--- loops.js
+++ loops.js
@@ -1,1 +1,9 @@
 // Do your work here
+
+var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+
+for(var i = 0; i < presidents.length; i++){
+  console.log("the value of the index is: " + presidents[i]);
+  console.log("the value of i is: " + i);
+}
+
