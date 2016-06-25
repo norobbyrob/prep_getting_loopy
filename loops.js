4dfcfbd3284a963c04aff3f51a87cff36fdc8feb
norobbyrob <nrobinson18@punahou.edu>
2016-06-24 14:50:47 -1000

    revised

--- loops.js
+++ loops.js
@@ -1,9 +1,13 @@
 // Do your work here
+function listPresidents(presidents){
+	for(var i = 0; i < presidents.length; i++){
+  		console.log("the value of the index is: " + presidents[i]);
+  		console.log("the value of i is: " + i);
+	}
+}
+
 
 var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+listPresidents(presidents);
 
-for(var i = 0; i < presidents.length; i++){
-  console.log("the value of the index is: " + presidents[i]);
-  console.log("the value of i is: " + i);
-}
 
