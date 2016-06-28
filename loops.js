320f2b62a8c4051734f9f41b7514657f52d0a133
norobbyrob <nrobinson18@punahou.edu>
2016-06-27 14:13:15 -1000

    put all in one

--- loops.js
+++ loops.js
@@ -1,4 +1,18 @@
-// Do your work here
+//Challenge 1
+function listPresidents(presidents){
+ 	for(var i = 0; i < presidents.length; i++){
+   		console.log("the value of the index is: " + presidents[i]);
+   		console.log("the value of i is: " + i);
+ 	}
+ }
+ 
+  
+  var presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'];
+  listPresidents(presidents);
+
+
+//Challenge 3
+
 var evenNumberArray = ''
 
 function evenNumbers(){
@@ -11,5 +25,4 @@ console.log("Value is: " + evenNumberArray)
 }
 
 evenNumbers();
-
 
